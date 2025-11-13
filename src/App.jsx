6ce220119cc1ffa6dbd0b7ff92066c2d37512911
giannivitale <giavitale78@gmail.com>
2025-11-13 12:47:01 +0100

    Add axios dependency and implement basic POST request in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 const enpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
+
+axios.post('endpoint')
+.then(response =>{
+
+const result = response.data;
+
+
+})
 
 const [formData, setFormData]= useState({ 
   author: " ",
