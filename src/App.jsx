8dd33cb3b865591ee3acb4d6576e46c3264647d1
giannivitale <giavitale78@gmail.com>
2025-11-13 15:51:10 +0100

    Refactor App component to handle form data and submit POST request using axios thanks to the teacher as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,88 @@
 import { useState } from 'react'
-import './App.css'
+
 import axios from 'axios';
 
-const enpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
-
-axios.post('endpoint')
-.then(response =>{
-
-const result = response.data;
-
-
-})
-
-const [formData, setFormData]= useState({ 
-  author: " ",
-  title: " ",
-  body: " ",
-  public: true
-
-
-
-});
+const endpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
 
 
 
 
-function handleFormData(e) {
-  const value = 
-    e.target.public === "checkbox" ? e.target.checked : e.target.value;
-    
+
+function App (){
+
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: true
+  });
+
+
+
+
+  function handleFormData(e) {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+const name = e.target.name
     setFormData({
-      ...formData, [e.target.name] : value,
+      ...formData, [name]: value,
 
     });
-  
-  
-  
   }
-  
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post(endpoint,formData)
+    .then(response =>{
+
+     const result = response.data;
+      console.log(result);
+      
+
+    })
+
+
+  }
+
+
+
+
   return (
     <>
-    <div className='container mt-4'>
+    
+      <div className='container mt-4'>
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+          name = "author"
+          value = {formData.author}
+          onChange = {handleFormData}
+          />
+         
 
-    name= "author"
-    checked={formData.author}
-    onChange={handleFormaData} 
-    id=""   
-    
+          
+        <button type='submit'>Conferma</button>
+        </form>
 
-    </div>
-    
-    
-    
-
+      </div>
 
     </>
   )
 
+}
+
+
+
+  
+  
+
 
 export default App
+
+
+
+    
+    
+    
+
