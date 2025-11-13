04f66426dea2b4d9798e0adb52827b09714420eb
giannivitale <giavitale78@gmail.com>
2025-11-13 12:38:40 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
-const enpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
+const enpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
+
+const [formData, setFormData]= useState({ 
+  author: " ",
+  title: " ",
+  body: " ",
+  public: true
+
+
+
+});
+
+
 
 
 function App() {
@@ -19,6 +31,13 @@ function App() {
   return (
     <>
      
+
+
+
+
+
+
+
     </>
   )
 }
